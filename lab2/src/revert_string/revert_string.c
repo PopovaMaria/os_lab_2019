0223e6cf07c26d12378f3a27415d1ef821300401
PopovaMaria <Geer78@ya.ru>
2020-03-20 11:46:05 +0300

    revert_string changed

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -7,20 +7,10 @@ void RevertString(char *str)
 {
     int length=strlen(str);
     char temp = 0;
-//    char *temp_str = malloc(sizeof(char) * length + 1);
-//    for(int i=length; i>=0; i--)
-//	{temp=length - i;
-//	temp_str[i]=str[temp];}
-//    for(int i=0; i<=length; i++)
-//	{str[i] = temp_str[i];}
-//    free(temp_str);
 for (int i = 0; i < length/2; ++i) 
 {
        temp = str[i];
        str[i] = str[length -1 - i];
        str[length -1 - i] = temp;
 }
-//temp = str[length-1];
-//str[length-1] = str[0];
-//str[0]=temp;
 }
